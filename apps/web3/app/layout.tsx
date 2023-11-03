302d990e0a9bd8119a297878835c3d0fc076e0a1
crypto0627 <jake0627a1@gmail.com>
2023-11-03 11:26:49 +0800

    feat: monorepo init

---
+++ apps/web3/app/layout.tsx
@@ -0,0 +1,26 @@
+import { cn } from '@/lib/utils'
+import './globals.css'
+
+import { Inter as FontSans } from "next/font/google"
+ 
+export const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+            <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+      >{children}</body>
+    </html>
+  )
+}
