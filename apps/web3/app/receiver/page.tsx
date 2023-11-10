40104dbcfb8861ad619b1b781feeddf538386f4d
crypto0627 <jake0627a1@gmail.com>
2023-11-10 10:26:20 +0800

    feat: navbar and footer

---
+++ apps/web3/app/receiver/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+import React from "react"
+import Link from "next/link"
+
+export default function Receiver(){
+    return (
+        <div>
+          <h1>First Post</h1>
+          <h2>
+            <Link href="/">Back to home</Link>
+          </h2>
+        </div>
+      );
+}
---
+++ apps/web3/components/Navbar.tsx
@@ -0,0 +1,60 @@
+'use client'
+import React from 'react'
+import { Disclosure } from '@headlessui/react'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+import Link from 'next/link'
+import Image from 'next/image'
+
+const navigation = [
+    { name: 'Provider', href: '/provider', current: true },
+    { name: 'Receiver', href: '/receiver', current: false },
+    { name: 'Food Bank', href: '/foodbank', current: false },
+]
+
+export default function Navbar() {
+    return (
+        <Disclosure as="nav" className="bg-gray-800">
+            {({ open }: any) => (
+                <>
+                    <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+                        <div className="relative flex h-16 items-center justify-between">
+                            <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                                {/* Mobile menu button*/}
+                                <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                                    <span className="absolute -inset-0.5" />
+                                    <span className="sr-only">Open main menu</span>
+                                    {open ? (
+                                        <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                                    ) : (
+                                        <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                                    )}
+                                </Disclosure.Button>
+                            </div>
+                            <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                                <div className="flex flex-shrink-0 items-center text-white">
+                                    <Image src='/logo.svg' alt="Logo" width={40} height={40} />
+                                    <Link href='/'>Yakitate</Link>
+                                </div>
+                                <div className="hidden sm:ml-6 sm:block">
+                                    <div className="flex space-x-4">
+                                        {navigation.map((item) => (
+                                            <a
+                                                key={item.name}
+                                                href={item.href}
+                                                className='text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium'
+                                                aria-current={item.current ? 'page' : undefined}
+                                            >
+                                                {item.name}
+                                            </a>
+                                        ))}
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </>
+            )}
+        </Disclosure>
+    )
+}
