226669b4619d6c68027fa4bf2410c765c374a50f
LinXJ1204 <39145342+LinXJ1204@users.noreply.github.com>
2023-11-12 12:49:33 +0800

    feat: algorandService & connect wallet

---
+++ apps/web3/interface.ts
@@ -0,0 +1,14 @@
+export type asset = {
+    amount: number,
+    "asset-id": number,
+    "is-frozen": boolean
+}
+
+export type food = {
+    amount: number,
+    id: number,
+    name: string,
+    provider: string,
+    total: number,
+    url: string
+}
