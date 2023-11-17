f02c05876a492ff1d93ac78cbd0a755caaea4fc7
crypto0627 <jake0627a1@gmail.com>
2023-11-17 14:25:34 +0800

    feat: provider css

--- apps/web3/components/Navbar.tsx
+++ apps/web3/components/Navbar.tsx
@@ -1,4 +1,3 @@
-'use client'
 import React from 'react'
 import { Disclosure } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
@@ -9,50 +8,33 @@ import { Wallet } from '@/service/perawallet'
 const navigation = [
     { name: 'Provider', href: '/provider', current: true },
     { name: 'Receiver', href: '/receiver', current: false },
-    { name: 'Food Bank', href: '/foodbank', current: false },
 ]
 
 export default function Navbar() {
     return (
-        <Disclosure as="nav" className="flex h-16 w-screen items-center justify-between bg-gray-800">
+        <Disclosure as="nav" className="flex h-16 w-screen bg-gray-800">
             {({ open }: any) => (
                 <>
-                    <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-                        <div className="relative flex h-16 items-center justify-between">
-                            <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                                {/* Mobile menu button*/}
-                                <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                                    <span className="absolute -inset-0.5" />
-                                    <span className="sr-only">Open main menu</span>
-                                    {open ? (
-                                        <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
-                                    ) : (
-                                        <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
-                                    )}
-                                </Disclosure.Button>
+                    <div className="px-2 sm:px-6 lg:px-8 w-full">
+                        <div className="flex h-16 items-center justify-between">
+                            <div className="flex items-center">
+                                <Image src='/logo.svg' alt="Logo" width={40} height={40} />
+                                <Link href='/' className="text-white ml-2">Yakitate</Link>
                             </div>
-                            <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-                                <div className="flex flex-shrink-0 items-center text-white">
-                                    <Image src='/logo.svg' alt="Logo" width={40} height={40} />
-                                    <Link href='/'>Yakitate</Link>
-                                </div>
-                                <div className="hidden sm:ml-6 sm:block">
-                                    <div className="flex space-x-4">
-                                        {navigation.map((item) => (
-                                            <a
-                                                key={item.name}
-                                                href={item.href}
-                                                className='text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium'
-                                                aria-current={item.current ? 'page' : undefined}
-                                            >
-                                                {item.name}
-                                            </a>
-                                        ))}
-                                    </div>
-                                </div>
-                                <div className='flex flex-1 items-center justify-center sm:items-stretch sm:justify-end'>
-                                    <Wallet/>
-                                </div>
+                            <div className="hidden sm:flex space-x-4">
+                                {navigation.map((item) => (
+                                    <a
+                                        key={item.name}
+                                        href={item.href}
+                                        className='text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium'
+                                        aria-current={item.current ? 'page' : undefined}
+                                    >
+                                        {item.name}
+                                    </a>
+                                ))}
+                            </div>
+                            <div className='flex items-center'>
+                                <Wallet />
                             </div>
                         </div>
                     </div>
